e868344624934303fe7dc4dc4f92adabebc944d5
Emilia Walendziuk <89015@student.pb.edu.pl>
2025-12-08 14:34:09 +0100

    v3.1

---
+++ HUMANITY 2/ModeSelectWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using HumanityWPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using System.Windows;
+
+namespace HumanityWPF
+{
+    public partial class ModeSelectWindow : Window
+    {
+        public ModeSelectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GraphicButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var gameWindow = new StartupWindow();
+            gameWindow.Show();
+
+            this.Close(); // zamknij okno wyboru
+        }
+
+        private void TextButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(
+                    @"C:\Users\DELL\Desktop\Humanity\bin\Debug\net8.0\Humanity.exe"
+                );
+
+                // opcjonalnie zamknij WPF, jeśli chcesz zostawić tylko konsolę
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nie udało się uruchomić wersji tekstowej: " + ex.Message);
+            }
+        }
+    }
+}
